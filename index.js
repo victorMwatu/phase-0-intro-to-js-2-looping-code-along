5ca199c333ab2308be0b85d3055e98e741b5ca8f
victorMwatu <victormwatu@gmail.com>
2025-05-29 13:36:41 +0300

    complete looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(gifters, occassion) {
+  const birthday = [];
+
+    for(let i = 0; i < gifters.length; i++) {
+        birthday.push(`Thank you, ${gifters[i]}, for the wonderful ${occassion} gift!`);
+        debugger;
+    }
+    return birthday;
+}
+// writeCards(["Charlie", "Samip", "Ali"], "birthday");
+// console.log(birthday);
+
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n);
+    n--;
+  }
+}
